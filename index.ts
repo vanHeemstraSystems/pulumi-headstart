f45ccf5527d5f218b1492a36c13ea93bf88721a5
wvanheemstra <wvanheemstra@users.noreply.github.com>
2022-07-01 10:49:55 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,53 @@
+
+import { chooseColor, outputFormatter } from './outputFormatter';
+import { runPulumiProject } from './runPulumiProject';
+
+interface ClusterOutputs {
+  shinyName: string;
+  shinyConfig: string;
+}
+
+async function main() {
+  const clusters = [
+    { name: 'kind-local', cloud: 'kind', nodePort: 32001 },
+  ];
+
+  await Promise.allSettled(
+    clusters.map(async (clusterDefinition) => {
+      let theme = chooseColor();
+
+      const serviceType = clusterDefinition.nodePort ? 'NodePort' : 'LoadBalancer';
+      const serviceNodePort = `${clusterDefinition.nodePort ?? 0}`;
+      const cluster = await runPulumiProject<ClusterOutputs>({
+        dir: './shinycluster',
+        project: 'shinycluster',
+        stackName: clusterDefinition.name,
+        operation: 'up',
+        additionalConfig: {
+          cloud: { value: clusterDefinition.cloud },
+          nodePort: { value: serviceNodePort },
+        },
+        formatter: outputFormatter(`cluster   ${clusterDefinition.name}`, theme)
+      });
+
+      if (!cluster) {
+        return;
+      }
+
+      runPulumiProject({
+        dir: './shinyapp',
+        project: 'shinyapp',
+        stackName: clusterDefinition.name,
+        operation: 'up',
+        additionalConfig: {
+          kubeconfig: cluster.outputs.shinyConfig,
+          serviceType: { value: serviceType },
+          serviceNodePort: { value: serviceNodePort },
+        },
+        formatter: outputFormatter(`app       ${clusterDefinition.name}`, theme)
+      });
+    }),
+  );
+}
+
+main();
